fd2158f9cc050e8870700517e99b58348b91cca8
Jakub Sopel <jakubsopel7@gmail.com>
2021-05-13 15:02:48 +0200

    class based views

--- rest_api/api/urls.py
+++ rest_api/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
 
 urlpatterns = [
-    path('people/', views.people_list),
-    path('people/<int:pk>/', views.people_detail)
+    path('people/', views.PeopleList.as_view()),
+    path('people/<int:pk>/', views.PeopleDetail.as_view())
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- rest_api/api/views.py
+++ rest_api/api/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
-from api.models import Person
-from api.serializers import PersonSerializer
+
+from django.http.response import Http404
 from rest_framework import viewsets
 from rest_framework import permissions
+
+from rest_framework import status
+from rest_framework import decorators 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from api.models import Person
+from api.serializers import PersonSerializer
 
 # Create your views here.
 
@@ -16,46 +20,85 @@ from rest_framework import permissions
 
 '''
 
-@csrf_exempt
-def people_list(request):
-    if request.method == 'GET':
+# Class based views
+class PeopleList(APIView):
+    def get(self, request, format=None):
         human = Person.objects.all()
         serializer = PersonSerializer(human, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = PersonSerializer(data=data)
+        return Response(serializer.data)
+    def post(self, request, format=None):
+        serializer = PersonSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-def people_detail(request,pk):
-    try:
-        human = Person.objects.get(id=pk)
-    except Person.DoesNotExist:
-        return HttpResponse(status=404)
+class PeopleDetail(APIView):
+    def get_object(self,pk):
+        try:
+           return Person.objects.get(pk=pk)
+        except Person.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
-        
+    def get(self,request,pk,format=None):
+        human = self.get_object(pk)
         serializer = PersonSerializer(human)
-        print("uzywa mojego widoku")
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
     
-    elif request.method == 'PUT':
-        
-        serializer = PersonSerializer(human, data=request.data, partial=True)
-        
+    def put(self, request, pk, format=None):
+        human = self.get_object(pk)
+        serializer = PersonSerializer(human, data=reqeust.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        return JsonResponse(serializer.errors, status = 400)
-    
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        human  = self.get_object(pk)
         human.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
-   
+
+##Function based views
+
+# @api_view(['GET', 'POST'])
+# def people_list(request, format=None):
+#     if request.method == 'GET':
+#         human = Person.objects.all()
+#         serializer = PersonSerializer(human, many=True)
+#         return Response(serializer.data)
+
+#     elif request.method == 'POST':
+        
+#         serializer = PersonSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=201)
+#         return Response(serializer.errors, status=400)
+
+# @api_view(['GET','PUT','DELETE'])
+# def people_detail(request,pk,format=None):
+#     try:
+#         human = Person.objects.get(pk=pk)
+#     except Person.DoesNotExist:
+#         return Response(status=404)
+
+#     if request.method == 'GET':
+        
+#         serializer = PersonSerializer(human)
+#         print("uzywa mojego widoku")
+#         return Response(serializer.data)
+    
+#     elif request.method == 'PUT':
+        
+#         serializer = PersonSerializer(human, data=request.data, partial=True)
+        
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+
+#         return Response(serializer.errors, status = 400)
+    
+#     elif request.method == 'DELETE':
+#         human.delete()
+#         return Response(status=204)

94ba325ef6beeb795ad01ea58980748f1e638831
Jakub Sopel <jakubsopel7@gmail.com>
2021-05-13 16:52:08 +0200

    api root, pagination, finally a working code snippet with highlights

--- rest_api/api/permissions.py
+++ rest_api/api/permissions.py
@@ -1,9 +1,9 @@
 from rest_framework import permissions
 
 class IsOwnerOrReadOnly(permissions.BasePermission): #class that allows only the snippet owner to edit it
-    
+
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-            return obj.owner == request.user
+        return obj.owner == request.user

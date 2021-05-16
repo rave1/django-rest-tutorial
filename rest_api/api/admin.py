dcdb1bd5b5575014bde12fd8b1f6eac9874427f0
Jakub Sopel <jakubsopel7@gmail.com>
2021-05-16 16:22:23 +0200

    Final version

--- rest_api/api/admin.py
+++ rest_api/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from api.models import Snippet
 
 # Register your models here.
+
+class SnippetAdmin (admin.ModelAdmin):
+    pass
+admin.site.register(Snippet, SnippetAdmin)
--- rest_api/api/views.py
+++ rest_api/api/views.py
@@ -8,8 +8,9 @@ from rest_framework import permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework.decorators import action
 from rest_framework import renderers
-
+from rest_framework import viewsets
 
 # Create your views here.
 
@@ -32,13 +33,19 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = SnippetSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
+
+
+# class UserViewSet(viewsets.ReadOnlyModelViewSet): #this class replaces UserList and UserDetail -> Nice
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+     queryset = User.objects.all()
+     serializer_class = UserSerializer
 
 @api_view(['GET'])
 def api_root(request, format=None):
